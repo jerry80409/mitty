fa82c8178494f2f11555a66522ceb9e3bb41d6f0
jerry80409 <jerry80409@gmail.com>
2021-01-09 22:18:05 +0800

    init

---
+++ yatagarasu/src/main/java/com/jerry80409/yatagarasu/YatagarasuApplication.java
@@ -0,0 +1,15 @@
+package com.jerry80409.yatagarasu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class YatagarasuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YatagarasuApplication.class, args);
+    }
+
+}
---
+++ yatagarasu/src/test/java/com/jerry80409/yatagarasu/YatagarasuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry80409.yatagarasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatagarasuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
