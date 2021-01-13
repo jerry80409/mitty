c8afa23ed8964eb205ec79e8dc49f1df8df3363d
jerry80409 <jerry80409@gmail.com>
2021-01-13 09:54:03 +0800

    algorithm

---
+++ algorithm/src/main/java/Fibonacci.java
@@ -0,0 +1,44 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fibonacci {
+    static Map<Integer, BigInteger> dp = new HashMap<>();
+
+    static {
+        dp.put(0, BigInteger.ZERO);
+        dp.put(1, BigInteger.ONE);
+        dp.put(2, BigInteger.ONE);
+        dp.put(3, BigInteger.TWO);
+        dp.put(4, BigInteger.valueOf(3L));
+        // ... init some com.example.practice.Fibonacci result
+    }
+
+    /**
+     * F(0) = 0
+     * F(1) = 1
+     * F(n) = F(n-1) + F(n-2)
+     *
+     * @param n
+     * @return
+     */
+    static BigInteger calculate(int n) {
+        // return cache value
+        if (dp.containsKey(n)) {
+            return dp.get(n);
+        }
+
+        if (n <= 0) {
+            return BigInteger.ZERO;
+        }
+        if (n == 1 || n == 2) {
+            return BigInteger.ONE;
+        }
+
+        // recursive and add to cache
+        dp.put(n, calculate(n - 1).add(calculate(n - 2)));
+
+        return dp.get(n);
+    }
+
+}
---
+++ algorithm/src/test/java/FibonacciTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciTest {
+
+    @Test
+    void assert_f0_zero() {
+        assertEquals(BigInteger.ZERO, Fibonacci.calculate(0));
+    }
+
+    @Test
+    void assert_f1_one() {
+        assertEquals(BigInteger.ONE, Fibonacci.calculate(1));
+    }
+
+    @Test
+    void assert_f2_one() {
+        assertEquals(BigInteger.ONE, Fibonacci.calculate(2));
+    }
+
+    @Test
+    void assert_f3_two() {
+        assertEquals(BigInteger.TWO, Fibonacci.calculate(3));
+    }
+
+    @Test
+    void assert_f10_fifty_five() {
+        assertEquals(BigInteger.valueOf(55L), Fibonacci.calculate(10));
+    }
+}
